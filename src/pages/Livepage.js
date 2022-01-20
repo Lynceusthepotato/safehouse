fac839bce155be550cfc6cf408bef75bfe85be12
Lynceusthepotato <55400836+Lynceusthepotato@users.noreply.github.com>
2022-01-20 08:43:47 +0700

    for the live

--- src/pages/Livepage.js
+++ src/pages/Livepage.js
@@ -1,8 +1,14 @@
+import Jitsi from "react-jitsi";
+
+
 
 const Livepage = () => {
+    const roomName = 'https://meet.jit.si/PervasiveFive'
+    const userFullName = 'SafeHouse'
+
     return (
         <div>
-            
+            <Jitsi roomName={roomName} displayName={userFullName} />
         </div>
     )
 }
